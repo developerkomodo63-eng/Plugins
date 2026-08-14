69e277fa9af491193d8dd6fa66c6541337cbd49b
developerkomodo63-eng <developerkomodo63@gmail.com>
2026-08-14 19:48:21 -0400

    Delete pedals-vst-main directory

--- pedals-vst-main/Pedals/Common/fast_tanh.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Lightweight rational approximation of tanh for high-performance per-sample use.
-// Good enough for audio shaping where perfect mathematical tanh isn't required.
-static inline float fast_tanh(float x) noexcept
-{
-    const float x2 = x * x;
-    return x * (27.0f + x2) / (27.0f + 9.0f * x2);
-}
